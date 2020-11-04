3f08133d3a2a533f51d4e57c8b225b5cbbaac869
Alex Kysov <48977515+pylsar@users.noreply.github.com>
2020-11-04 19:35:53 +0300

    scripts section 2 section 3

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-// $(document).ready(function(){
-//     $('.gallary__pics__box').slick({
-//         dots: true,
-//         infinite: true,
-//         speed: 300,
-//         slidesToShow: 1,
-//         adaptiveHeight: true
-//     });
-// });
+//слайдер
+$(document).ready(function(){
+    $('.gallary__pics__box').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+});
+
+
+//скролл
+$("#scrollImg").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+        scrollTop: $(".second").offset().top  // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+});
+
+// тогл меню
+$('#dropdown').on('click', function(){
+    $('.head__right__menu').toggleClass('menuShow');
+})
