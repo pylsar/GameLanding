d6947953b60b474ca84c4d67af0f1cc1c851f122
Alex Kysov <48977515+pylsar@users.noreply.github.com>
2020-11-15 20:26:45 +0300

    animation first page

--- script.js
+++ script.js
@@ -21,3 +21,14 @@ $("#scrollImg").click(function() { // ID откуда кливаем
 $('#dropdown').on('click', function(){
     $('.head__right__menu').toggleClass('menuShow');
 })
+
+
+
+// анимация футера
+let tl = new TimelineMax();
+
+tl
+    .fromTo('.head__logo', 1, {y: -100, opacity: 0}, {y: 0, opacity: 1})
+    .fromTo('.head__right', 1, {x: 200}, {x: 0}, '-=0.7')
+    .staggerFromTo('.head__navigation ul li a', 0.5, {y: 30, opacity: 0}, {y: 0, opacity: 1}, 0.05)
+    .staggerFromTo('h1 span', 0.3, {y: 40, opacity: 0}, {y: 0, opacity: 1}, 0.05)
